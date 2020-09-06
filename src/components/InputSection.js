5336689cc80008eb861f013188707582a46ee379
DELPHI METALS <myteax@gmail.com>
2020-09-06 15:37:31 -0400

    fixed the NaN error.

--- src/components/InputSection.js
+++ src/components/InputSection.js
@@ -10,19 +10,25 @@ import { red } from "@material-ui/core/colors";
 const InputSection = () => {
   const tt = useContext(GlobalContext);
   const [description, setDescription] = useState("");
-  const [amount, setAmount] = useState("");
+  const [amounts, setAmount] = useState("");
 
   const onsub = (e) => {
     e.preventDefault();
     let yy = {
       id: Math.random() * 10,
       text: description,
-      amount: amount,
+      amount: isNaN(amounts) ? 0 : amounts,
     };
-    tt.additem(yy);
-    setDescription("");
-    setAmount("");
-    document.getElementById("ttf").focus();
+    if (isNaN(amounts)) {
+      document.getElementById("spanny").style.visibility = "visible";
+      document.getElementById("ttf2").focus();
+    } else {
+      document.getElementById("spanny").style.visibility = "hidden";
+      tt.additem(yy);
+      setDescription("");
+      setAmount("");
+      document.getElementById("ttf").focus();
+    }
   };
 
   const useStyles = makeStyles({
@@ -31,23 +37,37 @@ const InputSection = () => {
       // psotion: "relative",
       left: "auto",
       padding: 5,
+      color: "red !important",
     },
     bbn: {
       left: "auto",
       margin: 5,
     },
+    // floatingLabelFocusStyle: {
+    //   color: "red",
+    // },
   });
   const classes = useStyles();
 
   return (
     <div>
       <form onSubmit={onsub} className="tpo" noValidate autoComplete="off">
+        <span id="spanny" style={{ visibility: "hidden", color: "#F24130" }}>
+          {" "}
+          Error!
+        </span>
         <div>
           {/* <label> Input Description</label>
           <br /> */}
           <TextField
             id="ttf"
             className={classes.texf}
+            InputLabelProps={{
+              style: { color: "#EFC9A5" },
+            }}
+            // InputLabelProps={{
+            //   className: classes.floatingLabelFocusStyle,
+            // }}
             value={description}
             type="text"
             onChange={(e) => setDescription(e.target.value)}
@@ -60,11 +80,16 @@ const InputSection = () => {
           {/* <label> Input Amount</label>
           <br /> */}
           <TextField
+            id="ttf2"
             className={classes.texf}
+            InputLabelProps={{
+              style: { color: "#EFC9A5" },
+              yy,
+            }}
             type="text"
-            value={amount}
+            value={amounts}
             onChange={(e) => setAmount(e.target.value)}
-            label="Amount"
+            label={isNaN(amounts) ? "Amount should be a number" : "Anount"}
             variant="filled"
           />
         </div>
